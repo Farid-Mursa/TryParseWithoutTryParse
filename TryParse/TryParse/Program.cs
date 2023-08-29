fd349a25c4bd2b9e4fec7bf77c2907de2c947ceb
Farid-Mursa <117182057+Farid-Mursa@users.noreply.github.com>
2023-08-29 13:38:02 +0400

    Add files via upload

---
+++ TryParse/TryParse/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TryParse
+{
+    internal class Program
+    {
+        static bool CustomTryParse(string input, out int result)
+        {
+
+            try
+            {
+                result = int.Parse(input);
+                return true;
+            }
+            catch
+            {
+                result = 0;
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isDigit = CustomTryParse(input, out int result);
+            Console.WriteLine(isDigit);
+            Console.WriteLine(result);
+        }
+    }
+}
